21cbf2c5197330c4be837f83a0ecd8510138f26c
Akeem <16595684+sulaimonao@users.noreply.github.com>
2024-12-31 13:19:30 -0800

    cleanup

--- lib/rename_dbcol_underscore.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE contexts RENAME COLUMN "userid" TO user_id;
-ALTER TABLE contexts RENAME COLUMN "chatroomid" TO chatroom_id;
-
-ALTER TABLE memories RENAME COLUMN "userid" TO user_id;
-ALTER TABLE memories RENAME COLUMN "chatroomid" TO chatroom_id;
-
-ALTER TABLE heads RENAME COLUMN "userid" TO user_id;
-ALTER TABLE heads RENAME COLUMN "chatroomid" TO chatroom_id;
--- lib/user_chatid_taskcar.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE task_cards ADD COLUMN user_id text;
-ALTER TABLE task_cards ADD COLUMN chatroom_id text;

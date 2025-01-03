98c675aa0a1de50db46e815f6be6ebceb4e23ac0
Akeem <16595684+sulaimonao@users.noreply.github.com>
2025-01-02 17:09:28 -0800

    Update index.js

--- src/util/index.js
+++ src/util/index.js
@@ -3,6 +3,7 @@ export { default as constants } from './constants.js';
 export { default as helpers } from './helpers.js';
 export { default as logger } from './logger.js';
 export { default as validation } from './validation.js';
+export { default as logError } from './logger.js';
 
 // Export individual db_helpers functions instead of the whole module
 export {

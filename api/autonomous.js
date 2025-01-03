1042174c297aae8e0416c309390b30c52c14272b
Akeem <16595684+sulaimonao@users.noreply.github.com>
2025-01-02 17:07:00 -0800

    Update autonomous.js

--- api/autonomous.js
+++ api/autonomous.js
@@ -2,7 +2,7 @@
 import { orchestrateContextWorkflow } from "../src/logic/index.js";
 import { fetchGaugeData, logInfo, logError } from "../src/util/index.js";
 
-export default async (req, res) => {
+module.exports = async (req, res) => {
   try {
     const { query, memory, logs, feedback, user_id, chatroom_id } = req.body;
 
